3742eeaa295b1ff035bcbcbb50838da63d3ba0f7
Maya Vateva <maya.vateva02@gmail.com>
2024-06-25 13:23:02 -0700

    progress

--- base.py
+++ base.py
@@ -5,7 +5,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/About Me')
+def about():
+    return render_template('AboutMe.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
 
-# djfkbgadkljfgad;lfkghd;lfghdsf;lgkhdsflkgdgit
