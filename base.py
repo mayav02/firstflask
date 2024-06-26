ac90d557d2f35082e9e743f87b7dc6fa8dd0fc1d
Maya Vateva <maya.vateva02@gmail.com>
2024-06-25 17:08:31 -0700

    attach pages

--- base.py
+++ base.py
@@ -5,9 +5,25 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/About Me')
+@app.route('/aboutme')
 def about():
-    return render_template('AboutMe.html')
+    return render_template('aboutme.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/beauty')
+def beauty():
+    return render_template('beauty.html')
+
+@app.route('/skincare')
+def skinare():
+    return render_template('skincare.html')
+
+@app.route('/wellness')
+def wellness():
+    return render_template('wellness.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
